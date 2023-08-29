70137db7be2118926ad7494f35961149ec574c8a
valljoa <joan.vallat@divtec.ch>
2023-08-29 16:21:48 +0200

    Codage du bouton ajouter

--- js/main.js
+++ js/main.js
@@ -8,13 +8,22 @@
 "use strict";
 
 
-//Récupérer tout les paragraphe du documents
+//Récupérer tout les paragraphe du documents et l'affiche dans la console
 let tableauDesPara = document.querySelectorAll("p");
-
-
-//Affiche le contenu des varaible dans la console
 console.log(tableauDesPara);
 
 
+//Récupère le bouton et l'affiche dans la console
 let btAjouter = document.querySelector("button.ajouter");
-console.log(btAjouter);
+let txtArticle = document.querySelector("#article");
+let ulArticles = document.querySelector("ul.articles");
+
+
+//Ecoute le click sur le bouton sur btAjouter
+btAjouter.addEventListener("click",()=> {
+
+    //Affiche la valeur du champs de saisie dans la console
+    ulArticles.innerHTML += "<li>" + txtArticle.value + "</li>";
+    //Vide la console
+    txtArticle.value = "";
+});
