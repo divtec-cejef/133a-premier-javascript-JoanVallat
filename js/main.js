fd4880493a2ce0a7fb5a0ce92a94dfe8b12e7975
valljoa <joan.vallat@divtec.ch>
2023-08-29 15:29:32 +0200

    Création du fichier JS et affichage de bonjour dans la console

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Fichier de base pour application web HTML/CSS/JS
+ * @author :    Joan Vallat
+ * @since :     2023-08-29
+ * @version :   0.0.1
+ */
+
+
+//Création de
+let message = "Bonjour";
+
+console.log(message);
